1df0ee72adad3877432a47a9cade9bd6f4630c5b
TechTrapture <143475073+vishal-bulbule@users.noreply.github.com>
2024-04-25 17:07:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -6,3 +6,11 @@ resource "google_storage_bucket" "my-bucket" {
   public_access_prevention = "enforced"
 }
 #
+resource "google_storage_bucket" "my-bucket2" {
+  name                     = "tt-githubdemo-bucket-002"
+  project                  = "tt-dev-001"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
+}
+#
